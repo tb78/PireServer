d2d53a55c8d0282a62762cfde1dd91dea9743097
UnknownSKL <degekste@jimkroon.nl>
2012-02-13 16:17:43 +0100

    Added SiriServer plugin.

---
+++ plugin/webservice.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+from datetime import date
+import locale 
+from plugin import *
+
+from urllib import urlopen
+import urllib
+import urllib2
+import json
+
+from siriObjects.baseObjects import AceObject, ClientBoundCommand
+from siriObjects.uiObjects import AddViews, AssistantUtteranceView
+from siriObjects.systemObjects import DomainObject
+
+class webservicePlugin(Plugin):   
+
+    @register("en-US", ".*")
+    def do_webrequest(self, speech, language):
+        
+        # Edit this adress.
+        pireLocation = 'http://siri.devfusion.nl/response.php'
+        
+        location = self.getCurrentLocation(force_reload=True)
+        
+        postdata = {
+        	'text': speech,
+        	'language': language,
+        	'latitude': str( location.latitude ),
+        	'longitude': str( location.longitude )
+        };
+        
+        req = urllib2.Request( pireLocation + "?" + urllib.urlencode( postdata ),
+			headers = {
+     		"Content-Type": "application/json",
+			"Accept": "*/*",   
+        	"User-Agent": "pire-plugin", 
+       	})
+       	
+        print '^^^^^^^^^^^^'
+        print 'Language: ' + language
+        print 'Lat: ' + str( location.latitude )
+        print 'Long: ' + str( location.longitude )
+        print '^^^^^^^^^^^^'
+       	
+        webjson = urllib2.urlopen(req).read()
+        request = json.loads(webjson)
+
+        view = AddViews(self.refId, dialogPhase="Summary")
+        view.views = request
+        print view.to_plist()
+        self.sendRequestWithoutAnswer(view.to_plist())
+        self.complete_request()
